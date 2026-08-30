758a725b799c02198ef3643ce9b59c99187b2460
Kesha-Babriya <keshababriya21@gmail.com>
2026-08-30 17:06:44 +0530

    Modify file handling

--- Stage-01/05-file-handling/file-handle.py
+++ Stage-01/05-file-handling/file-handle.py
@@ -1,15 +1,15 @@
 ##Create student.txt using Python and store:
 
-# create mode - 'x'     if file exist gives error
-# with open('student.txt','x') as file:
-#     file.write("Hello make file from python file \nHello from Keshaaa!!!")
+#create mode - 'x'     if file exist gives error
+with open('student.txt','x') as file:
+    file.write("Hello make file from python file \nHello from Keshaaa!!!")
 
 
-# list1 = ['\nNoah\n','Nick\n','Conny\n']
-# with open('student.txt','a') as file:
-#     file.writelines(list1)            #writelines write list
+list1 = ['\nNoah\n','Nick\n','Conny\n']
+with open('student.txt','a') as file:
+    file.writelines(list1)            #writelines write list
 
-#read mode
+# read mode
 
 file = open('student.txt','r')
 data = file.read()      #read all file data 
